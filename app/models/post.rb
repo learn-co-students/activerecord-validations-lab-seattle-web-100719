108dcf73a871cfdadf8b3eb1d8f8626ebb35d36d
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-06 13:12:33 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 10 }
+    validates :summary, length: { maximum: 10 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :non_clickbait
+
+    def non_clickbait
+        if self.title != nil && !(self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top [number]") || self.title.include?("Guess"))
+            errors.add(:expiration_date, "not clickbaity enough")
+        end
+    end
 end
